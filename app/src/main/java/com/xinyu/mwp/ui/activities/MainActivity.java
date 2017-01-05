81150c412d1f1616b8c2e46c77db910f960f1a05
TangBuzhi <tj_code@163.com>
2017-01-05 14:22:13 +0800

    1-5 侧滑菜单OK+首页轮播图OK

--- app/src/main/java/com/xinyu/mwp/ui/activities/MainActivity.java
+++ app/src/main/java/com/xinyu/mwp/ui/activities/MainActivity.java
@@ -1,11 +1,11 @@
 package com.xinyu.mwp.ui.activities;
 
-import android.media.tv.TvContract;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -14,13 +14,18 @@ import android.widget.TextView;
 
 import com.xinyu.mwp.R;
 import com.xinyu.mwp.base.BaseActivity;
+import com.xinyu.mwp.base.BaseAdapter;
+import com.xinyu.mwp.bean.DrawerSettingBean;
+import com.xinyu.mwp.ui.adapter.DrawerSettingAdapter;
 import com.xinyu.mwp.ui.fragments.ExchangeFragment;
 import com.xinyu.mwp.ui.fragments.HomeFragment;
 import com.xinyu.mwp.ui.fragments.ShareFragment;
+import com.xinyu.mwp.utils.SpaceItemDecoration;
 import com.xinyu.mwp.utils.ToastUtil;
 import com.xinyu.mwp.view.CircleImageView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import butterknife.BindView;
@@ -46,17 +51,31 @@ public class MainActivity extends BaseActivity {
     LinearLayout mExchangeLayout;
     @BindView(R.id.share_layout)
     LinearLayout mShareLayout;
+    @BindView(R.id.tv_assets)
+    TextView mAssets;
+    @BindView(R.id.tv_grade)
+    TextView mGrade;
+    @BindView(R.id.rcv_drawer_list)
+    RecyclerView mDrawerList;
 
     public static HomeFragment mHomeFragment;
     public static ExchangeFragment mExchangeFragment;
     public static ShareFragment mShareFragment;
-    private List<View> mViews = new ArrayList<>();
+    private List<View> mViews;
+    private List<String> mSettings;
+    private List<DrawerSettingBean> mDatas;
+
+    private int[] settingImgs = new int[]{R.mipmap.ic_drawer_attention, R.mipmap.ic_drawer_push_bill, R.mipmap.ic_drawer_share_bill,
+            R.mipmap.ic_drawer_exchange_detail, R.mipmap.ic_drawer_comments, R.mipmap.ic_drawer_products_grade, R.mipmap.ic_drawer_focus};
+    private DrawerSettingAdapter mSettingAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initActinbar(mToolBar, mDrawer);
+        initView();
+        initListener();
     }
 
     @Override
@@ -68,12 +87,37 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void initView() {
+        /**
+         * 首页tab导航页
+         */
+        mViews = new ArrayList<>();
         mHomeFragment = new HomeFragment();
         getSupportFragmentManager().beginTransaction().add(R.id.container, mHomeFragment).commitAllowingStateLoss();
         mHomeLayout.setSelected(true);
         mViews.add(mHomeLayout);
         mViews.add(mExchangeLayout);
         mViews.add(mShareLayout);
+
+        /**
+         * 侧滑菜单栏
+         */
+        mAssets.setText(String.format(getString(R.string.my_assets_num), "￥16202.00"));
+        mGrade.setText(String.format(getString(R.string.my_grade_num), "123"));
+        mDatas = new ArrayList<>();
+        mSettings = Arrays.asList(getResources().getStringArray(R.array.drawer_setting));
+        int size = mSettings.size();
+        for (int i = 0; i < size; i++) {
+            DrawerSettingBean settingBean = new DrawerSettingBean();
+            settingBean.setIcon(settingImgs[i]);
+            settingBean.setText(mSettings.get(i));
+            mDatas.add(settingBean);
+        }
+        mDrawerList.setLayoutManager(new LinearLayoutManager(context));
+        mDrawerList.setHasFixedSize(true);
+        int spacingInPixels = getResources().getDimensionPixelSize(R.dimen.space);
+        mDrawerList.addItemDecoration(new SpaceItemDecoration(spacingInPixels));
+        mSettingAdapter = new DrawerSettingAdapter(context, mDatas, R.layout.item_drawer_setting);
+        mDrawerList.setAdapter(mSettingAdapter);
     }
 
     @Override
@@ -82,6 +126,18 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void initListener() {
+        mSettingAdapter.setOnItemClickListener(new BaseAdapter.onItemClickListener() {
+            @Override
+            public void onItemClick(View itemView, int position) {
+                // TODO: 2017/1/5 跳转到详情界面待定。。。
+                ToastUtil.showToast(mSettings.get(position), context);
+            }
+
+            @Override
+            public void onItemLongClick(View itemView, int position) {
+
+            }
+        });
     }
 
     @OnClick({R.id.view_head, R.id.home_layout, R.id.exchange_layout, R.id.share_layout})
@@ -90,8 +146,8 @@ public class MainActivity extends BaseActivity {
         switch (view.getId()) {
             case R.id.view_head:  //进入注册页面
                 //ToastUtil.showToast("注册", context);
-               // toActivity(RegisterActivity.class);
-                toActivity(LogonActivity.class,false);
+                // toActivity(RegisterActivity.class);
+                toActivity(LogonActivity.class, false);
                 break;
             case R.id.home_layout:
                 if (mHomeFragment == null) {
--- app/src/main/java/com/xinyu/mwp/ui/fragments/HomeFragment.java
+++ app/src/main/java/com/xinyu/mwp/ui/fragments/HomeFragment.java
@@ -5,10 +5,17 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.xinyu.mwp.R;
 import com.xinyu.mwp.base.BaseFragment;
+import com.xinyu.mwp.utils.PixUtil;
+import com.xinyu.mwp.utils.ToastUtil;
+import com.xinyu.mwp.view.AutoScrollViewPager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 
@@ -21,15 +28,84 @@ import butterknife.BindView;
 public class HomeFragment extends BaseFragment {
     private static final String TAG = "HomeFragment";
 
-    @BindView(R.id.tv)
-    TextView tv;
+    @BindView(R.id.view_banner)
+    LinearLayout mBanner;
+    @BindView(R.id.ll_dots)
+    LinearLayout mDots;
+
+    private int[] banners = new int[]{R.mipmap.banner1, R.mipmap.banner2, R.mipmap.banner3,
+            R.mipmap.banner4, R.mipmap.banner5, R.mipmap.banner6, R.mipmap.banner7};
+    private AutoScrollViewPager mViewPager;
+    private List<ImageView> imgViews;
+    private List<ImageView> imgDots;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         setContentView(R.layout.fragment_home);
-        tv.setText(this.getClass().getSimpleName());
+        initView();
+        initData(null);
+        initListener();
         return parentView;
+    }
+
+    @Override
+    public void initView() {
+        /**
+         * 首页轮播图
+         */
+        imgViews = new ArrayList<>();
+        imgDots = new ArrayList<>();
+        int length = banners.length;
+        for (int i = 0; i < length; i++) {
+            ImageView view = new ImageView(context);
+            view.setImageResource(banners[i]);
+            view.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imgViews.add(view);
+        }
+        imgDots = new ArrayList<>();
+        initDots(length);
+        mViewPager = new AutoScrollViewPager(context);
+        mViewPager.setImageView(imgViews);
+        mViewPager.setDots(imgDots);
+        mViewPager.start();
+        mBanner.addView(mViewPager);
+    }
+
+    private void initDots(int size) {
+        imgDots.clear();
+        mDots.removeAllViews();
+        for (int i = 0; i < size; i++) {
+            ImageView dot = new ImageView(context);
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(PixUtil.dip2px(context, 8), PixUtil.dip2px(context, 8));
+            if (i == 0) {
+                dot.setBackgroundResource(R.drawable.dot_focused);
+            } else {
+                params.leftMargin = PixUtil.dip2px(context, 8);
+                dot.setBackgroundResource(R.drawable.dot_normal);
+            }
+            dot.setLayoutParams(params);
+            imgDots.add(dot);
+            mDots.addView(dot);
+        }
+    }
+
+    @Override
+    public void initData(Object data) {
+
+    }
+
+    @Override
+    public void initListener() {
+        /**
+         * 轮播图点击监听
+         */
+        mViewPager.setOnItemClickListener(new AutoScrollViewPager.onItemClickListener() {
+            @Override
+            public void onClick(int position) {
+                ToastUtil.showToast("点击了第" + (position + 1) + "个条目", context);
+            }
+        });
     }
 }
